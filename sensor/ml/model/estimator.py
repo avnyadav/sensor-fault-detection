58ea1013543002c70134086720677aab529123d2
Avnish327030 <anishyadav7045075175@gmail.com>
2022-10-30 13:50:41 +0530

    updated

--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -10,3 +10,15 @@ class TargetValueMapping:
     def reverse_mapping(self):
         mapping_response = self.to_dict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
+
+
+#Write a code to train model and check the accuracy.
+
+class SensorModel:
+
+
+    def get_best_model():
+        pass
+
+
+
