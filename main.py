0770f13f4b2fc44eadd5a30477d24c2cc4e2f2bf
Avnish Yadav <34875169+avnyadav@users.noreply.github.com>
2022-11-05 19:28:32 +0530

    added  requirement.txt

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ from fastapi.responses import Response
 from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
 from sensor.utils.main_utils import load_object
 from fastapi.middleware.cors import CORSMiddleware
-env_file_path="/config/workspace/env.yaml"
+import os
+
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
 
 def set_env_variable(env_file_path):
-    if os.environ['MONGO_DB_URL'] is None:
+
+    if os.getenv('MONGO_DB_URL',None) is None:
         env_config = read_yaml_file(env_file_path)
         os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 
@@ -85,5 +88,5 @@ def main():
 
 if __name__=="__main__":
     #main()
-    set_env_variable(env_file_path)
+    # set_env_variable(env_file_path)
     app_run(app, host=APP_HOST, port=APP_PORT)
