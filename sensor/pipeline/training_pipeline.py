128f9ffb396132d5c981fa4e80f4061393a34d36
Avnish Yadav <34875169+avnyadav@users.noreply.github.com>
2022-11-05 23:19:32 +0530

    Update training_pipeline.py

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -13,6 +13,8 @@ from sensor.components.model_evaluation import ModelEvaluation
 from sensor.components.model_pusher import ModelPusher
 from sensor.cloud_storage.s3_syncer import S3Sync
 from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR
+
 class TrainPipeline:
     is_pipeline_running=False
     def __init__(self):
@@ -89,6 +91,13 @@ class TrainPipeline:
             self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_buket_url=aws_buket_url)
         except Exception as e:
             raise SensorException(e,sys)
+            
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_buket_url)
+        except Exception as e:
+            raise SensorException(e,sys)
 
     def run_pipeline(self):
         try:
@@ -105,6 +114,7 @@ class TrainPipeline:
             model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
             TrainPipeline.is_pipeline_running=False
             self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
         except  Exception as e:
             self.sync_artifact_dir_to_s3()
             TrainPipeline.is_pipeline_running=False
